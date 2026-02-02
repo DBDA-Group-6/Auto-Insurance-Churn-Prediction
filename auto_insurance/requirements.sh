24d2631dfed5f4bd812fae3225192f2fbe5d2409
DBDA-Group-6 <zukiroina@gmail.com>
2026-02-02 14:04:17 +0530

    Update requirements.sh
    removed pyspark installation

--- auto_insurance/requirements.sh
+++ auto_insurance/requirements.sh
@@ -3,7 +3,8 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y python3 python3-pip python3-dev gcc g++ make openjdk-11-jdk
 pip3 install --user --break-system-packages flask pandas pymysql sqlalchemy
 sudo apt install -y mysql-client
-pip3 install --user --break-system-packages joblib scikit-learn xgboost imbalanced-learn pyspark
+pip3 install --user --break-system-packages joblib scikit-learn xgboost imbalanced-learn
 source ~/.bashrc
 python3 -c "import flask, pandas, sklearn, xgboost, imblearn, pyspark; print('ML + Web + PySpark ready!')"
 mysql --version
+
